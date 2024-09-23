0275edda41ec11cf43b0bb5eb59185f87cc29704
raphaelsantiago <raphaelsantiago.sg@example.com>
2024-09-23 13:51:50 +0800

    Initial Commit

---
+++ knowledge-app/jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+  displayName: 'knowledge-app',
+  preset: './jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: './coverage/knowledge-app',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+  ],
+};
